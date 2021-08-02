ef4c525b19b7be9c6af6d6d9db06c84e38e522ff
gkaan <gkaan@daum.net>
2021-08-02 15:32:10 +0900

    [21.08.02 #js 산술, 대입, 비교 연산자]

---
+++ 1. core-js/2. 데이터 타입/3. 기타 기본타입.js
@@ -0,0 +1,24 @@
+
+//논리타입
+var logical = false; // 논리 타입: 소문자 true, false 
+console.log(typeof logical);
+
+
+// logical = True;
+
+
+//udefined타입 -> undefined값: 변수를 초기화하지(설정하지) 않았을 때
+var hello;
+console.log(hello);
+
+// null -> 의도적으로 없는 값
+var apple ='사과';
+apple = null;
+
+var hello = saying => {
+    if (saying ==='즐') return null;
+    else return "안뇽~";
+}
+
+console.log("hello" * "메롱");
+
