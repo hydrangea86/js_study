f6885b7ea2266931696802bf00e69197191cef1e
gkaan <gkaan@daum.net>
2021-08-03 15:52:43 +0900

    [21.08.03 #js 논리, 조건, if ]

---
+++ 1. core-js/4. 제어문/1. 조건문/1. if-else.js
@@ -0,0 +1,15 @@
+
+var point = 30;
+console.log(`점수: ${point}점`);
+
+// if 사용시 {중괄호} 안할 시 한 문장 코드만 실행
+if (point >= 60) {
+    console.log(`시험에 합격했습니다!`);
+    console.log(`축하합니다!!`);
+}
+else {
+    console.log(`응 불합격~~`);
+    console.log(`수고링`);
+}
+
+console.log(`즐거운 하루 되세요~!`);
